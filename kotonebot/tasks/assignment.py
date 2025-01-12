ac455348b40d27ef384419d083f3287863548a41
XcantloadX <3188996979@qq.com>
2025-01-12 12:42:22 +0800

    fix(task): 修复分配工作时无法选中位于第二页的好调偶像

--- kotonebot/tasks/assignment.py
+++ kotonebot/tasks/assignment.py
@@ -22,7 +22,7 @@ def acquire_assignment():
         sleep(5)
 
 @action('重新分配工作')
-def assign(type: Literal['mini', 'online']):
+def assign(type: Literal['mini', 'online']) -> bool:
     """
     分配工作
 
@@ -32,28 +32,56 @@ def assign(type: Literal['mini', 'online']):
     :param type: 工作类型。mini=ミニライブ 或 online=ライブ配信。
     """
     # [kotonebot/tasks/assignment.py]
+    image.expect_wait(R.Daily.IconTitleAssign, timeout=10)
     if type == 'mini':
-        device.click(image.expect(R.Daily.IconAssignMiniLive))
+        if image.find(R.Daily.IconAssignMiniLive):
+            device.click()
+        else:
+            logger.warning('MiniLive already assigned. Skipping...')
+            return False
     elif type == 'online':
-        device.click(image.expect(R.Daily.IconAssignOnlineLive))
+        if image.find(R.Daily.IconAssignOnlineLive):
+            device.click()
+        else:
+            logger.warning('OnlineLive already assigned. Skipping...')
+            return False
     else:
         raise ValueError(f'Invalid type: {type}')
     # MiniLive/OnlineLive 页面 [screenshots/assignment/assign_mini_live.png]
     image.expect_wait(R.Common.ButtonSelect, timeout=5)
-    results = image.find_many(R.Daily.IconAssignKouchou, threshold=0.8)
-    results.sort(key=lambda r: tuple(r.position))
-    results.pop(0) # 第一个是说明文字里的图标
     # 选择好调偶像
-    for target in results:
-        with cropped(device, y2=0.4):
-            img1 = device.screenshot()
-            target = results.pop()
-            device.click(target)
-            sleep(1)
-            img2 = device.screenshot()
-            if image.raw().similar(img1, img2):
-                logger.info(f'Idol #{target} already assigned. Trying next.')
-                continue
+    selected = False
+    max_attempts = 4
+    attempts = 0
+    while not selected:
+        # 寻找所有好调图标
+        results = image.find_many(R.Daily.IconAssignKouchou, threshold=0.8)
+        results.sort(key=lambda r: tuple(r.position))
+        results.pop(0) # 第一个是说明文字里的图标
+        # 尝试点击所有目标
+        for target in results:
+            with cropped(device, y2=0.3):
+                img1 = device.screenshot()
+                target = results.pop()
+                # 选择偶像并判断是否选择成功
+                device.click(target)
+                sleep(1)
+                img2 = device.screenshot()
+                if image.raw().similar(img1, img2, 0.97):
+                    logger.info(f'Idol #{target} already assigned. Trying next.')
+                    continue
+                selected = True
+                break
+        if not selected:
+            attempts += 1
+            if attempts >= max_attempts:
+                logger.warning('Failed to select kouchou idol. Keep using the default idol.')
+                break
+            # 说明可能在第二页
+            device.swipe_scaled(0.6, 0.7, 0.2, 0.7)
+            sleep(0.5)
+        else:
+            break
     # 点击选择
     sleep(0.5)
     device.click(image.expect(R.Common.ButtonSelect))
@@ -71,7 +99,7 @@ def assign(type: Literal['mini', 'online']):
     device.click(confirm)
     # 点击 開始する [screenshots/assignment/assign_mini_live3.png]
     device.click(image.expect_wait(R.Common.ButtonStart, timeout=5))
-
+    return True
 
 @task('工作')
 def assignment():
@@ -105,5 +133,6 @@ if __name__ == '__main__':
     # # 领取完后会自动进入分配页面
     # image.expect_wait(R.Daily.IconAssignMiniLive, timeout=7)
     # assign_mini_live()
+    assign('mini')
     assign('online')
-
+    # device.swipe_scaled(0.6, 0.7, 0.2, 0.7)
